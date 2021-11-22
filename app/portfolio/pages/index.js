d583d0e33c01dca0bb9c2ae6afd903315d824332
Mothapo-Thapelo <dev.mothapotr@gmail.com>
2021-11-22 16:37:13 +0200

    layout changes

--- app/portfolio/pages/index.js
+++ app/portfolio/pages/index.js
@@ -24,26 +24,42 @@ export default function Home() {
       <main className={styles.main}>
 
         <section className={styles.intro}>
-          <img src="/profile-img.jpg" alt="Picture of Thapelo"/>
+            <img src="/profile-img.jpg" alt="Picture of Thapelo" className={styles.profile}/>
 
-          <h1>Hi ABC team</h1>
-          <h2>I am Thapelo.</h2>
-          <p>I enjoy playing the piano, but I am still learning though, hear for yourself.  </p>
-          <audio src="/test-tune.wav" controls></audio>
+            <div className={styles.icontent}>
+              <h1>Hi <span>Abc</span> team</h1>
+              <h2>I am Thapelo.</h2>
+              <p>I enjoy playing the piano, but I am still learning though, hear for yourself. </p>
+              <audio src="/test-tune.wav" controls ></audio>
+            </div>
 
-          <p className={styles.introclear}>I am a -year-old pioneer in software deve  lopment, especially client-side development. I find that interacting with the user of the system I am designing and developing for fulfilling when I see amazement written on their face.</p>
-
-          <p >The need for designing and developing accessible, beautiful, interactive, and efficient systems is what keeps me at night researching and learning better ways to lift the restriction on how users access to content and products we provide.</p>
         </section>
 
-        <hr className={styles.divider}/>
+        <section className={styles.mybackground}>
+
+            <div className={styles.section}>
+              <p >I am a -year-old pioneer in software development, especially client-side development. I find that interacting with the user of the system I am designing and developing for fulfilling when I see amazement written on their face.</p>
+              <img src="https://i1.wp.com/media4.giphy.com/media/26BkNyQ3ovYKrJTcQ/giphy.gif" alt=""/>
+            </div>
+         
+
+            <div className={styles.section}>
+              <img src="https://cdn.prod.openfit.com/uploads/2017/01/How-to-Lose-Weight-if-You-Work-the-Night-Shift.jpg" alt=""/>
+
+              <p >The need for designing and developing accessible, beautiful, interactive, and efficient systems is what keeps me at night researching and learning better ways to lift the restriction on how users access the content and products we provide.</p>
+            </div>
+           
+        </section>
+
         <section className={styles.aboutcontent}>
-          <h3>Why work at ABC?</h3> 
+          <h2>I need to work @ Abc because:</h2> 
           <p>Honestly speaking, I am attracted and sold the most by Sovtech's motto of developing custom solutions. I support and believe in the idea of giving organizations of different sizes a unique identity online as it does make a difference in allowing them to better connect and communicate with their users. This is a motto I can stand behind and help fulfill it.</p>
 
           <p>Looking at the Sovtech clients' section there are a lot of familiar brands, most I use daily. This brings a lot of curiosity and excitement when I think about being part of a team that designs and develops systems that help these brands serve us better as consumers of content and products.</p>
 
           <p>With the above said, I believe being part of the Sovtech team will allow me to grow as a designer/developer, in the process changing my overall world view on designing and developing systems that serve clients and their users better.</p>
+
+       
         </section>
 
     
